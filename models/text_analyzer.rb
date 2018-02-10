dcc35de20d24aa6e20d5c8f34d7e89105cd1c33a
Andy Maskiell <maskiella@gmail.com>
2018-02-10 20:04:10 +0000

    automatically backed up by learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -23,6 +23,19 @@ class TextAnalyzer
 
     arr = sq.split('')
     arr1 = arr.uniq
-  end
+    arr2 = {}
 
+    arr1.map do |c|
+      arr2[c] = arr.count(c)
+    end
+
+    biggest = { arr2.keys.first => arr2.values.first }
+
+    arr2.each do |key, value|
+      if
+      end
+    end
+
+    biggest
+  end
 end
